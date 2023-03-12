193a1917cffdffc0de62f99bbc241a16186cd6b2
Lucien Brun <lucien.brun@epitech.eu>
2023-03-12 19:59:34 +0100

    Init

---
+++ App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import {RootNavigator} from './src/navigation';
+
+function App(): JSX.Element {
+  return <RootNavigator />;
+}
+
+export default App;
